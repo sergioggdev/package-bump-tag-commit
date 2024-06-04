651872a1745dd42515312a52aa6264bb3dfc6e85
Sergio Garcia <sergio.garcia.seo@gmail.com>
2024-06-04 17:33:47 +0200

    changes

--- dist/index.js
+++ dist/index.js
@@ -34,7 +34,7 @@ module.exports = class GitCmd {
     await exec('git', ['config', '--local', 'user.email', 'info@conectaturismo.com']);
     await exec('git', ['commit', '--no-verify', '-m', 'CI: Publish new version']);
     // await exec('git', ['remote', 'add', 'origin', repo]);
-    await exec('git', ['push', 'origin', github.context.ref.slice(2).join('/')]);
+    await exec('git', ['push', 'origin', github.context.ref.split('/').slice(2).join('/')]);
     console.log('Finish!!');
   }
 };
